5c5223127f3e4b7120fcde93aa0d19b766dbb8ac
Bruno Franciscon Mazzotti <bruno.mazzotti@gmail.com>
2015-04-10 23:04:03 -0300

    Plot 2 implemented.

--- plot2.R
+++ plot2.R
@@ -9,4 +9,12 @@ plot.2 <- function(data)
         data <- get.data()
     }
 
+    Sys.setlocale("LC_TIME", "English")
+
+    png(filename="plot2.png")
+
+    with(data, plot(DateTime, Global_active_power, type="l",
+                    ylab="Global Active Power (kilowatts)", xlab=""))
+
+    dev.off()
 }
